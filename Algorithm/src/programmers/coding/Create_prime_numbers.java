102e1ae2755be79d6d1640b9f5bd6e8be560bc6f
uuoodong <dev.udong.top@gmail.com>
2022-04-11 18:19:23 +0900

    20220411

---
+++ Algorithm/src/programmers/coding/Create_prime_numbers.java
@@ -0,0 +1,39 @@
+package programmers.coding;
+
+public class Create_prime_numbers {
+
+	class Solution {
+		int answer;
+
+		void prime(int x) {
+			if (x == 0)
+				return;
+			if (x == 1)
+				return;
+			for (int i = 2; i <= (int) Math.sqrt(x); i++) {
+				if (x % i == 0)
+					return;
+			}
+			answer++;
+		}
+
+		public int solution(int[] nums) {
+			answer = 0;
+			for (int i = 0; i < nums.length - 2; i++) {
+				for (int j = i + 1; j < nums.length - 1; j++) {
+					for (int k = j + 1; k < nums.length; k++) {
+						prime(nums[i] + nums[j] + nums[k]);
+					}
+				}
+			}
+
+			return answer;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
