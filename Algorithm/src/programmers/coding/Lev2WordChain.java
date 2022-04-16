15e2d295cb5a5547a7862d85400e3e0fad997131
uuoodong <dev.udong.top@gmail.com>
2022-04-16 22:27:11 +0900

    20220416

--- Algorithm/src/programmers/coding/Lev2WordChain.java
+++ Algorithm/src/programmers/coding/Lev2WordChain.java
@@ -1,1 +1,32 @@
+package programmers.coding;
+import java.util.*;
+public class Lev2WordChain {
 
+	class Solution {
+	    public int[] solution(int n, String[] words) {
+	        int[] answer = new int[2];
+	        List<String> list = new ArrayList<>();
+	        for(int i=0; i<words.length; i++){
+	            if(list.contains(words[i])){
+	                answer[0] = (i%n) +1;
+	                answer[1] = (i/n) +1;
+	                break;
+	            }
+	            list.add(words[i]);
+	            if(i>0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
+	                answer[0] = (i%n) +1;
+	                answer[1] = (i/n) +1;
+	                break;
+	            }
+	        }
+	        
+	        return answer;
+	    }
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
