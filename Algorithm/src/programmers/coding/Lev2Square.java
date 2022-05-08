2cb9b2a68822e8e6c8e74d07f78108d0b758c12c
uuoodong <dev.udong.top@gmail.com>
2022-05-08 18:01:17 +0900

    20220508

---
+++ Algorithm/src/programmers/coding/Lev2Square.java
@@ -0,0 +1,16 @@
+package programmers.coding;
+
+public class Lev2Square {
+
+	class Solution {
+	    public long solution(int w, int h) {
+	        long answer = 1;
+	        return answer;
+	    }
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
