081711cec21102d285aa1f89ed2c53b8ff208b31
uuoodong <dev.udong.top@gmail.com>
2022-05-19 15:27:15 +0900

    20220519

--- Algorithm/src/leetCode/Title0067.java
+++ Algorithm/src/leetCode/Title0067.java
@@ -2,6 +2,28 @@ package leetCode;
 
 public class Title0067 {
 
+	class Solution {
+		public String addBinary(String a, String b) {
+			StringBuilder ans = new StringBuilder();
+			int i = a.length() - 1;
+			int j = b.length() - 1;
+			int sum = 0;
+			while (sum > 0 || i >= 0 || j >= 0) {
+				if (i >= 0) {
+					sum += a.charAt(i) - '0';
+					i--;
+				}
+				if (j >= 0) {
+					sum += b.charAt(j) - '0';
+					j--;
+				}
+				ans.append(sum % 2);
+				sum /= 2;
+			}
+			return ans.reverse().toString();
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
